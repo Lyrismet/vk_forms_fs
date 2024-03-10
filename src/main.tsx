bc37a882f3e53d83cccc65ed89f6082a788b827f
Lyrismet <ana0203tir@gmail.com>
2024-03-10 13:53:40 +0300

    feat: initial base structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import {
     AdaptivityProvider,
     ConfigProvider
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import App from './App';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-
+const queryClient = new QueryClient();
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
     <ConfigProvider>
         <AdaptivityProvider>
-            <App />
+            <QueryClientProvider client={queryClient}>
+                <App/>
+            </QueryClientProvider>
         </AdaptivityProvider>
     </ConfigProvider>,
 );
